690fb90ad84d0c51e505e52e23cb13ab32e0c4df
arvin <chenghao@ip-sky.cn>
2017-10-30 11:52:13 +0800

    Select支持inner join连接

--- Operate/Select.php
+++ Operate/Select.php
@@ -22,6 +22,7 @@ class Select extends Operate
     private $groupByColumns  = [];
     private $orderByInfo     = [];
     private $whereConditions = [];
+    private $innerJoinInfo   = [];
     private $lJoinInfo       = [];
     private $rJoinInfo       = [];
     private $forUpdate       = false;
@@ -58,6 +59,12 @@ class Select extends Operate
         return $this;
     }
 
+    public function join(Table $table, Condition ...$conditions)
+    {
+        $this->innerJoinInfo[] = [$table, $conditions];
+        return $this;
+    }
+
     public function lJoin(Table $table, Condition ...$conditions)
     {
         $this->lJoinInfo[] = [$table, $conditions];
@@ -68,6 +75,120 @@ class Select extends Operate
     {
         $this->rJoinInfo[] = [$table, $conditions];
         return $this;
+    }
+
+    private function innerJoinStr()
+    {
+        return self::createJoinStr($this->innerJoinInfo, '');
+    }
+
+    private function lJoinStr()
+    {
+        return self::createJoinStr($this->lJoinInfo, 'LEFT');
+    }
+
+    private function rJoinStr()
+    {
+        return self::createJoinStr($this->rJoinInfo, 'RIGHT');
+    }
+
+    private function innerJoinConditionValueArr()
+    {
+        $conditionArr = array_column($this->innerJoinInfo, 1);
+        return self::createConditionValueArr($conditionArr);
+    }
+
+    private function lJoinConditionValueArr()
+    {
+        $conditionArr = array_column($this->lJoinInfo, 1);
+        return self::createConditionValueArr($conditionArr);
+    }
+
+    private function rJoinConditionValueArr()
+    {
+        $conditionArr = array_column($this->rJoinInfo, 1);
+        return self::createConditionValueArr($conditionArr);
+    }
+
+    private function whereConditionStr()
+    {
+        if (!empty($this->whereConditions)) {
+            return 'WHERE ' . self::createConditionArrStr($this->whereConditions);
+        }
+        return '';
+    }
+
+    private function whereConditionValueArr()
+    {
+        return self::createConditionValueArr($this->whereConditions);
+    }
+
+    public function groupByColStr()
+    {
+        if (empty($this->groupByColumns)) {
+            return '';
+        }
+        $colStrArr = [];
+        foreach ($this->groupByColumns as $column) {
+            $colStrArr[] = (string)$column;
+        }
+        $colStr = implode(',', $colStrArr);
+        return "GROUP BY ($colStr) ";
+    }
+
+    public function groupBy(Column ...$cols)
+    {
+        $this->groupByColumns = array_merge($this->groupByColumns, $cols);
+        return $this;
+    }
+
+    public function orderByStr()
+    {
+        if (empty($this->orderByInfo)) {
+            return '';
+        } else {
+            return 'ORDER BY ' . implode(',', $this->orderByInfo);
+        }
+    }
+
+    public function orderBy(Column $col, bool $asc = true)
+    {
+        $this->orderByInfo[] = "`{$col->colName()}`" . ($asc ? ' ASC' : ' DESC');
+    }
+
+    public function limit(int $start, int $end)
+    {
+        $this->limitStart = $start;
+        $this->limitEnd   = $end;
+    }
+
+    public function prepareStr()
+    {
+        $tablesStr     = $this->table;
+        $selectColStr  = $this->createSelectColStr();
+        $joinStr       = $this->innerJoinStr();
+        $lJoinStr      = $this->lJoinStr();
+        $rJoinStr      = $this->rJoinStr();
+        $whereStr      = $this->whereConditionStr();
+        $groupByColStr = $this->groupByColStr();
+        $orderByStr    = $this->orderByStr();
+        $preStr        = "SELECT $selectColStr FROM $tablesStr $joinStr $lJoinStr $rJoinStr $whereStr $groupByColStr $orderByStr";
+        if (is_int($this->limitStart) && is_int($this->limitEnd)) {
+            $preStr = "$preStr limit $this->limitStart,$this->limitEnd";
+        }
+        if ($this->forUpdate) {
+            $preStr = "$preStr FOR UPDATE";
+        }
+        return $preStr;
+    }
+
+    public function prepareValues()
+    {
+        $innerConditionValues = $this->innerJoinConditionValueArr();
+        $lConditionValues     = $this->lJoinConditionValueArr();
+        $rConditionValues     = $this->rJoinConditionValueArr();
+        $whereConditionValues = $this->whereConditionValueArr();
+        return array_merge($innerConditionValues, $lConditionValues, $rConditionValues, $whereConditionValues);
     }
 
     private static function createConditionArrStr(array $conditionArr)
@@ -112,109 +233,8 @@ class Select extends Operate
             $conditionArr  = $lJoinItem[1];
             $conditionStr  = self::createConditionArrStr($conditionArr);
             $tableName     = (string)$lJoinItem[0];
-            $lJoinStrArr[] = " $joinDirection JOIN $tableName ON $conditionStr ";
+            $lJoinStrArr[] = "$joinDirection JOIN $tableName ON $conditionStr";
         }
-        return implode('', $lJoinStrArr);
-    }
-
-    private function createLJoinStr()
-    {
-        return self::createJoinStr($this->lJoinInfo, 'LEFT');
-    }
-
-    private function createLJoinConditionValueArr()
-    {
-        $conditionArr = array_column($this->lJoinInfo, 1);
-        return self::createConditionValueArr($conditionArr);
-    }
-
-    private function createRJoinConditionValueArr()
-    {
-        $conditionArr = array_column($this->rJoinInfo, 1);
-        return self::createConditionValueArr($conditionArr);
-    }
-
-    private function createRJoinStr()
-    {
-        return self::createJoinStr($this->rJoinInfo, 'RIGHT');
-    }
-
-    private function createWhereConditionStr()
-    {
-        if (!empty($this->whereConditions)) {
-            return 'WHERE ' . self::createConditionArrStr($this->whereConditions);
-        }
-        return '';
-    }
-
-    private function createWhereJoinConditionValueArr()
-    {
-        return self::createConditionValueArr($this->whereConditions);
-    }
-
-    public function createGroupByColStr()
-    {
-        if (empty($this->groupByColumns)) {
-            return '';
-        }
-        $colStrArr = [];
-        foreach ($this->groupByColumns as $column) {
-            $colStrArr[] = (string)$column;
-        }
-        $colStr = implode(',', $colStrArr);
-        return "GROUP BY ($colStr) ";
-    }
-
-    public function groupBy(Column ...$cols)
-    {
-        $this->groupByColumns = array_merge($this->groupByColumns, $cols);
-        return $this;
-    }
-
-    public function createOrderByStr()
-    {
-        if (empty($this->orderByInfo)) {
-            return '';
-        } else {
-            return 'ORDER BY ' . implode(',', $this->orderByInfo);
-        }
-    }
-
-    public function orderBy(Column $col, bool $asc = true)
-    {
-        $this->orderByInfo[] = "`{$col->colName()}`" . ($asc ? ' ASC' : ' DESC');
-    }
-
-    public function limit(int $start, int $end)
-    {
-        $this->limitStart = $start;
-        $this->limitEnd   = $end;
-    }
-
-    public function prepareStr()
-    {
-        $tablesStr     = $this->table;
-        $selectColStr  = $this->createSelectColStr();
-        $lJoinStr      = $this->createLJoinStr();
-        $rJoinStr      = $this->createRJoinStr();
-        $whereStr      = $this->createWhereConditionStr();
-        $groupByColStr = $this->createGroupByColStr();
-        $orderByStr    = $this->createOrderByStr();
-        $preStr        = "SELECT $selectColStr FROM $tablesStr $lJoinStr $rJoinStr $whereStr $groupByColStr $orderByStr";
-        if (is_int($this->limitStart) && is_int($this->limitEnd)) {
-            $preStr = "$preStr limit $this->limitStart,$this->limitEnd";
-        }
-        if ($this->forUpdate) {
-            $preStr = "$preStr FOR UPDATE";
-        }
-        return $preStr;
-    }
-
-    public function prepareValues()
-    {
-        $lConditionValues     = $this->createLJoinConditionValueArr();
-        $rConditionValues     = $this->createRJoinConditionValueArr();
-        $whereConditionValues = $this->createWhereJoinConditionValueArr();
-        return array_merge($lConditionValues, $rConditionValues, $whereConditionValues);
+        return implode(' ', $lJoinStrArr);
     }
 }
