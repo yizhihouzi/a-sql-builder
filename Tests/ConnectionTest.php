ea6e3ecde5c6982e5478f52a438622a2937e0e33
arvin <chenghao@ip-sky.cn>
2018-08-13 23:15:26 +0800

    test code opt

--- Tests/ConnectionTest.php
+++ Tests/ConnectionTest.php
@@ -12,6 +12,7 @@ use DBOperate\Column;
 use DBOperate\Condition;
 use DBOperate\Connection;
 use DBOperate\Exception\DBOperateException;
+use DBOperate\Operate\Delete;
 use DBOperate\Operate\Insert;
 use DBOperate\Operate\Select;
 use DBOperate\Operate\Update;
@@ -20,6 +21,7 @@ use PHPUnit\Framework\TestCase;
 
 class ConnectionTest extends TestCase
 {
+
     /**
      * @throws DBOperateException
      */
@@ -47,30 +49,61 @@ class ConnectionTest extends TestCase
         $insert->setInsertValues(['content'], [1], [2], [3]);
         $affNum = $conn->insert($insert);
         self::assertEquals(3, $affNum);
+        return $conn;
     }
 
     /**
+     * @depends testInsert
+     *
+     * @param Connection $conn
+     *
+     * @return int
      * @throws DBOperateException
      */
-    public function testUpdate()
+    public function testSelect(Connection $conn)
+    {
+        $select = new Select(new Table('t'));
+        $select->where(new Condition(new Column('id'), 1, '>'));
+        $select->orderBy(new Column('id'), false);
+        $select->fetchCols(new Column('content'), new Column('id'));
+        $row = $conn->select($select, true);
+        self::assertArrayHasKey('content', $row);
+        self::assertArrayHasKey('id', $row);
+        return $row['id'];
+    }
+
+    /**
+     * @depends testSelect
+     *
+     * @param $id
+     *
+     * @return int
+     * @throws DBOperateException
+     */
+    public function testUpdate(int $id)
     {
         $conn     = new Connection(DB_URI);
         $update   = new Update(new Table('t'));
         $content2 = 'hhh';
         $update->setColumn(new Column('content'), $content2);
-        $update->where(new Condition(new Column('id'), 2));
+        $update->where(new Condition(new Column('id'), $id));
         $affNum = $conn->update($update);
         self::assertEquals(1, $affNum);
+        return $id;
     }
 
-    public function testSelect()
+    /**
+     * @param int $id
+     *
+     * @depends testUpdate
+     * @throws DBOperateException
+     */
+    public function testDelete(int $id)
     {
         $conn   = new Connection(DB_URI);
-        $select = new Select(new Table('t'));
-        $select->where(new Condition(new Column('id'), 2));
-        $select->fetchCols(new Column('content'));
-        $row2     = $conn->select($select, true);
-        $content2 = 'hhh';
-        self::assertEquals($content2, $row2['content']);
+        $delete = new Delete(new Table('t'));
+        $delete->where(new Condition(new Column('id'), $id));
+        $affNum = $conn->delete($delete);
+        self::assertEquals(1, $affNum);
     }
 }
